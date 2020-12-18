bc643685d7680dc7d2f8df7593f7098fb72acd73
Knouy <SanquirgoTanguy@Gmail.Com>
2020-12-18 12:20:15 +0100

    master

--- food.sql
+++ food.sql
@@ -22,6 +22,8 @@ CREATE TABLE leaves
     id INT NOT NULL AUTO_INCREMENT
     ,start DATETIME NOT NULL
     ,end DATETIME NOT NULL
+    ,full_day VARCHAR(9) NOT NULL
+    ,reason VARCHAR(255)
     ,id_employee INT NOT NULL
     ,PRIMARY KEY (id)
     ,FOREIGN KEY (id_employee) REFERENCES employee (id)
@@ -58,7 +60,7 @@ AFTER INSERT ON leaves
 FOR EACH ROW
 BEGIN
     UPDATE employee
-    SET used = used + DATEDIFF(new.end, new.start)
+    SET used = used + TIMEDIFF(new.end, new.start) / 240000
     WHERE id = new.id_employee;
     CALL refresh(new.id_employee);
 END;
@@ -67,14 +69,15 @@ AFTER DELETE ON leaves
 FOR EACH ROW
 BEGIN
     UPDATE employee
-    SET used = used - DATEDIFF(old.end, old.start)
+    SET used = used - TIMEDIFF(old.end, old.start) / 240000
     WHERE id = old.id_employee;
     CALL refresh(old.id_employee);
 END
 //
 DELIMITER ;
 INSERT INTO employee VALUES (NULL, "Sanquirgo", "Tanguy", "tanguy.sanquirgo@foo-d.fr", "1998-12-15", "France", "Boulogne-Billancourt", "92100", "2020-10-07", "temp", (DATEDIFF(CURDATE(), "2020-10-07") * 2.5) / 28, 0);
-INSERT INTO leaves VALUES (NULL, "2020-12-24 00:00:00", "2020-12-26 00:00:00", 1);
+INSERT INTO leaves VALUES (NULL, "2020-12-15 12:00:00", "2020-12-16 00:00:00", "Afternoon", "Birthday party", 1);
+INSERT INTO leaves VALUES (NULL, "2020-12-24 00:00:00", "2020-12-26 00:00:00", "Yes", "Christmas holidays", 1);
 SELECT * FROM employee;
 SELECT * FROM leaves;
 SELECT login("tanguy.sanquirgo@foo-d.fr", "temp");
